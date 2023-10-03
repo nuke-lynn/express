2b39eae849b0c3f7f1f5ddfdb621f4e67d043acd
nuke-lynn <146854715+nuke-lynn@users.noreply.github.com>
2023-10-03 22:37:12 +0700

    Create demo expressJs app with finexo free template

--- index.js
+++ index.js
@@ -9,3 +9,37 @@
 'use strict';
 
 module.exports = require('./lib/express');
+const express = require('./lib/express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+    res.render('index', { title: 'Express Index Project' });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', { title: 'Express Index Project' });
+});
+
+app.get('/service', (req, res) => {
+    res.render('service', { title: 'Express Index Project' });
+});
+
+app.get('/why', (req, res) => {
+    res.render('why', { title: 'Express Index Project' });
+});
+
+app.get('/team', (req, res) => {
+    res.render('team', { title: 'Express Index Project' });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
